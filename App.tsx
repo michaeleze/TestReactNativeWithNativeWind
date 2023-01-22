efeff414e263093a3f8073ae0c65c4a4ffdcdefa
michael <michaeleze3@gmail.com>
2023-01-22 22:34:06 +0100

    init configs

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import "./styles.css";
+
+// const styles = {
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// }
+
+// const Styles = StyleSheet.create(styles);
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white" >
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View >
+  );
+};
